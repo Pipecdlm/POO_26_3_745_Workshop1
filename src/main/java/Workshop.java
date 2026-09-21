04726fdb2087331900aafbffea9d5f4ed7412307
Pipecdlm <afortiza@udistrital.edu.co>
2026-09-21 16:44:37 -0500

    tALLER FINAL

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -283,6 +283,6 @@ else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) signo = "Virgo"
 else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) signo = "Libra";
 else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) signo = "Escorpio";
 else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) signo = "Sagitario";
-else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) signo = "Capricornio" +"return signo;
+else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) signo = "Capricornio" +"return signo";
 }
 }
